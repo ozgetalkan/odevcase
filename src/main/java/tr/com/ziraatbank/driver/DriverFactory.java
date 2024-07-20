6e7102425e452164cac5897cd0ee676ecaf2882e
ozgetalkan <ozgetanulkualkan@gmail.com>
2024-07-21 02:54:38 +0300

    [added] Projeye DriverFactory eklendi

---
+++ src/main/java/tr/com/ziraatbank/driver/DriverFactory.java
@@ -0,0 +1,45 @@
+package tr.com.ziraatbank.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import tr.com.ziraatbank.utils.ConfigurationReader;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+    static WebDriver driver;
+    static Properties properties;
+
+    public static WebDriver initialize_Driver(String browser){
+
+        properties = ConfigurationReader.getProperties();
+        if (browser.equals("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equals("Firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            WebDriverManager.edgedriver().setup();    //Bos gonderirse
+            driver = new EdgeDriver();
+        }
+        String url = properties.getProperty("BaseUrl");
+        int impWait = Integer.parseInt(properties.getProperty("implicityWait"));
+        int pageWait = Integer.parseInt(properties.getProperty("pageLoadTimeout"));
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(impWait, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(pageWait, TimeUnit.SECONDS);
+        return getDriver();
+
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+
+}
